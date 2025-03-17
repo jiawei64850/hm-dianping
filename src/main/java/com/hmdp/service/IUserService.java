5d1f6fcfec26e2441a6d47e867a4047e06ef41e7
jiawei <karwailau64850@gmail.com>
2025-03-17 09:31:40 +0000

    短信验证码登陆功能业务代码实现

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -1,6 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 
@@ -16,5 +17,19 @@ import javax.servlet.http.HttpSession;
  */
 public interface IUserService extends IService<User> {
 
+    /**
+     * 发送验证码
+     * @param phone
+     * @param session
+     * @return
+     */
     Result sendCode(String phone, HttpSession session);
+
+    /**
+     * 用户登陆
+     * @param loginForm
+     * @param session
+     * @return
+     */
+    Result login(LoginFormDTO loginForm, HttpSession session);
 }
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.SystemConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +26,12 @@ import javax.servlet.http.HttpSession;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
-    @Override
+    /**
+     * 发送验证码
+     * @param phone
+     * @param session
+     * @return
+     */
     public Result sendCode(String phone, HttpSession session) {
         // 1. validate the phone number
         if (RegexUtils.isPhoneInvalid(phone)) {
@@ -39,5 +46,51 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         log.debug("发送短信验证码成功, 验证码: {}", code);
         // 6. return ok
         return Result.ok();
+    }
+
+    /**
+     * 用户登陆
+     * @param loginForm
+     * @param session
+     * @return
+     */
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 1. validate phone and code
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误！");
+        }
+        String cacheCode = session.getAttribute("code").toString();
+        String code = loginForm.getCode();
+        if (cacheCode == null || !code.equals(cacheCode)) {
+            // 2. return error message if not
+            return Result.fail("验证码错误！");
+        }
+        // 3. query the user whether in database or not (if validation pass)
+        User user = query().eq("phone", phone).one();
+        // 4. register to new user if not exist in database
+        if (user == null) {
+            user = createUserWithPhone(phone);
+        }
+        // 5. store information of user into session (whatever is new or not)
+        session.setAttribute("user", user);
+        // 6. return ok
+        return Result.ok();
+    }
+
+    /**
+     * 根据手机号注册新用户
+     * @param phone
+     * @return
+     */
+    private User createUserWithPhone(String phone) {
+        // 1. create user object
+        User user = new User();
+        // 2. set the attribute
+        user.setPhone(phone);
+        user.setNickName(SystemConstants.USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        // 3. store the user into database
+        save(user);
+        return user;
     }
 }
