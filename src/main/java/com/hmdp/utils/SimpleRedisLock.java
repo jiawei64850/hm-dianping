6e294eba04ae03d192d476966c591948eeeddd44
jiawei <karwailau64850@gmail.com>
2025-03-19 07:52:09 +0000

    解决基于redis分布式锁误删除业务代码实现

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.BooleanUtil;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -17,10 +18,11 @@ public class SimpleRedisLock implements ILock {
     }
 
     public static final String KEY_PREFIX = "lock:";
+    public static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
     @Override
     public boolean tryLock(long timeOutSec) {
-        // 1. get the id of thread as value
-        String value = String.valueOf(Thread.currentThread().getId());
+        // 1. get the id of thread and UUID as value (thread identification)
+        String value = ID_PREFIX + Thread.currentThread().getId();
         // 2. get the mutex
         String key = KEY_PREFIX + name;
         Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(key, value, timeOutSec, TimeUnit.SECONDS);
@@ -29,7 +31,15 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unlock() {
-        // release the mutex
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // get the identification of thread
+        String value = ID_PREFIX + Thread.currentThread().getId();
+        // get the identification of mutex
+        String key = KEY_PREFIX + name;
+        String id = stringRedisTemplate.opsForValue().get(key);
+        // check the same or not
+        if (value.equals(id)) {
+            // release the mutex
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
